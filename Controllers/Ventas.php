c2968be3bf1181fcfb08046338c3f7971880c1c6
barronFreddy <freddybarron7834@gmail.com>
2021-09-30 16:49:47 -0400

    modificando controlador Ventas

--- Controllers/Ventas.php
+++ Controllers/Ventas.php
@@ -1,136 +1,158 @@
 <?php
     class Ventas extends Controller{
-        protected $totalPagar, $tot = 0;
-        public function __construct()
-        {
+    public function __construct(){
         session_start();
-        if (empty($_SESSION['activo'])) {
-            header("location: " . base_url());
-        }
-            parent::__construct();
-
-        }
-        public function Listar()
-        {        
-            $this->views->getView($this, "Listar");
-        }
-        public function lista()
-        {
-            $data = $this->model->selectVentas();
-            $this->views->getView($this, "ListarVentas", $data, "");
-        }
-        public function ingresar()
-        {
-            $id = $_POST['id'];
-            $nombre = $_POST['nombre'];
-            $cantidad = $_POST['cantidad'];
-            $precio = $_POST['precio'];
-            $total = $cantidad * $precio;
-            $id_usuario = $_SESSION['id'];
-            $existe = $this->model->buscarProductoexiste($id, $id_usuario);
-            if ($existe) {
-                $idP = $existe['id'];
-                $cant = $existe['cantidad'];
-                $cantidad = $_POST['cantidad'] + $cant;
-                $total = $existe['precio'] * $cantidad;
-                $this->model->actualizarCantidad($cantidad,$total ,$idP);
-                echo "1";
-            }else{
-                $insert = $this->model->insertarDetalle($nombre, $cantidad, $precio, $total,$id,$id_usuario);
-                if ($insert > 0) {
-                $this->Listar();
-                }else{
-                echo "error";
-                }
-            }
-        }
-        public function ventas()
-        {
-            $data = $this->model->selectDetalle();
-            foreach ($data as $ventas) {
-            $this->totalPagar = $this->totalPagar + $ventas['total']; 
-            echo "<tr>
-                <td>".$ventas['id']."</td>
-                <td>".$ventas['nombre']."</td>
-                <td class='verificarCantidad'>" . $ventas['cantidad'] . "</td>
-                <td>" . $ventas['precio'] . "</td>
-                <td>" . $ventas['total'] . "</td>
-                <td>
-                <button typu='button' class='btn btn-danger' onclick='EliminarDetalle(" . $ventas['id'] . ");'>Eliminar</button>
-                </td>
-            </tr>";
-            
-            }
-            $tot = number_format($this->totalPagar, 2, ".", ",");
-
-            echo "<input type='hidden' id='totalPagar' value='" . $tot. "'/>";
-        }
-    
-    public function eliminar()
-    {
-        
-        $id = $_POST['id'];
-        $this->model->eliminarVentas($id);
+        parent::__construct();
     }
-    public function buscar()
+    public function index()
     {
-        $codigo = $_POST['codigo'];
-        $data = $this->model->buscarProducto($codigo);
-        echo json_encode($data);
+        $data = $this->model->getClientes();
+        $this->views->getView($this, "index", $data);
     }
-    public function registarVenta()
+    public function buscarCodigo($codigo)
     {
-        $data = $this->model->buscaridC();
-        $result = $data['MAX(id)'];
-        if ($result == null) {
-            $id_venta = 1;
-        }else{
-            $id_venta = $result;
-        }
-        
-    }
-    public function registrar()
-    {
-        $cliente = $_POST['cliente'];
-        $totalP = $_POST['totalP'];
-        if ($cliente == 0 || $cliente == "") {
-            # code...
-            $this->model->registrarCompra(1, $totalP);
-        }else{
-            $this->model->registrarCompra($cliente, $totalP);
-        }
-        $data = $this->model->buscaridC();
-        $result = $data['MAX(id)'];
-        $productos = $this->model->verificarProductos($_SESSION['id']);
-        foreach ($productos as $data) {
-            $stock = $this->model->stockActual($data['id_producto']);
-            $stockActual = $stock['cantidad'];
-            $insertar = $this->model->registrarDetalle($result, $data['nombre'] ,$data['id_producto'], $data['cantidad'], $data['precio'], $data['id_usuario']);
-            $this->model->registrarStock($stockActual - $data['cantidad'], $data['id_producto']);
-        }
-        $this->model->VaciarCompra($_SESSION['id']);
+        $data = $this->model->buscarProductoCodigo($codigo);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        //print_r($data);
         die();
     }
-    public function ver()
+    public function ingresarVenta()
     {
-        $config = $this->model->selectConfiguracion();
-        $id = $_GET['id'];
-        $id_cliente = $_GET['id'];
-        $data = $this->model->ListaVentas($id);
-        $cliente = $this->model->PdfCliente($id_cliente);
-        $this->views->getView($this, "VerVentas", $data ,"",$config, $cliente);
-    }
-    public function buscarCliente()
-    {
-        $return = array();
-        $data = $this->model->selectClientes();
-        if (!empty($data)) {
-            foreach ($data as $clientes) {
-                $res['id'] = $clientes['id'];
-                $res['value'] = $clientes['nombre'];
-                array_push($return, $res);
+        //print_r($_POST);
+        $id=$_POST['idProducto'];
+        $datos = $this->model->getProductos($id);
+        $idProducto = $datos['idProducto'];
+        $idUsuario =$_SESSION['idUsuario'];
+        $precio = $datos['precioVenta'];
+        $cantidad = $_POST['cantidad'];
+       
+        $comprobar = $this->model->consultarDetalleVenta($idProducto, $idUsuario);
+        if (empty($comprobar)) {
+            $subTotal = $precio*$cantidad;
+            $data = $this->model->registrarDetalle($idProducto,$idUsuario,$precio,$cantidad,$subTotal);
+            if ($data == "ok") {
+                $msg = array('msg' => 'Producto ingresado con exito!!', 'icono'=> 'success');
+            }else{
+                $msg = array('msg' => 'error!!', 'icono'=> 'error');
+            }
+        }else{
+            $TotalCant = $comprobar['cantidad'] + $cantidad;
+            $subTotal = $TotalCant*$precio;
+            $data = $this->model->actualizarDetalleVenta($precio,$TotalCant,$subTotal, $idProducto,$idUsuario);
+            if ($data == "modificado") {
+                $msg = array('msg' => 'Producto actualizado con exito!!', 'icono'=> 'success');
+            }else{
+                $msg = array('msg' => 'Error!!', 'icono'=> 'error');
             }
         }
-        echo json_encode($return);
+        echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+        die();
+    }
+    public function listarVenta()
+    {
+        $idUsuario =$_SESSION['idUsuario'];
+        $data['detalle'] = $this->model->getDetalle($idUsuario);
+        $data['subTotal'] = $this->model->calcularVenta($idUsuario);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        die();
+    }
+    public function eliminarVenta($id)
+    {
+        $data = $this->model->eliminarDetalle($id);
+        if ($data == 'ok') {
+            $msg = array('msg' => 'Producto eliminado!!', 'icono'=> 'success');
+        }else{
+            $msg = array('msg' => 'Error!!', 'icono'=> 'error');
+        }
+        echo json_encode($msg);
+        die();
+    }
+    public function registrarVentas($idCliente)
+    {
+        $idUsuario =$_SESSION['idUsuario'];
+        $total = $this->model->calcularVenta($idUsuario);
+        $data = $this->model->registrarVenta($idCliente, $total['total']);
+        if ($data == "ok") {
+            $detalle = $this->model->getDetalle($idUsuario);
+            $idVenta = $this->model->idVenta();
+            foreach ($detalle as $row) {
+                $cantidad = $row['cantidad'];
+                $precio = $row['precio'];
+                $idProducto = $row['idProducto'];
+                $subTotal = $cantidad * $precio;
+                $this->model->registrarDetalleVenta($idVenta['id'], $idProducto, $cantidad, $precio,$subTotal);
+                $stockActual = $this->model->getProductos($idProducto);
+                $stock = $stockActual['stock'] - $cantidad;
+                $this->model->actualizarStock($stock,$idProducto);
+            }
+            $vaciar = $this->model->vaciarDetalleVenta($idUsuario);
+            if ($vaciar == 'ok') {
+                $msg = array('msg' => 'ok', 'idVenta' => $idVenta['id']);
+            }
+        }else{
+            $msg = "error al registrar";
+        }
+        echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+        die();
+    }
+    public function generarPDF($idVenta)
+    {
+        $productos = $this->model->getProductosVenta($idVenta);
+        
+        //print_r($productos);
+        //exit;
+        require('libraries/fpdf/fpdf.php');
+
+        $pdf = new FPDF('P', 'mm', 'A4');
+        $pdf->AddPage();
+        $pdf->SetMargins(10,0,0);
+        $pdf->SetTitle('ReporteVentas');
+        $pdf->SetFont('Arial','',10);
+        $pdf->Cell(200, 10, 'REPORTES', 0, 1, 'C');
+        $pdf->Ln();
+
+        //encabezado de productos de
+        $pdf->SetFillColor(50,54,57);
+        $pdf->SetTextColor(255,255,255);
+        $pdf->cell(45, 7, 'Fecha Venta','TBLR', 0, 'C', true);
+        $pdf->cell(50, 7,'Cliente', 'TBLR', 0, 'C', true);
+        $pdf->cell(30, 7, utf8_decode('Descripción'),'TBLR', 0, 'C', true);
+        $pdf->cell(18, 7,'Cantidad', 'TBLR', 0, 'C', true);
+        $pdf->cell(25, 7,'Precio Cant.','TBLR', 0, 'C', true);
+        $pdf->cell(20, 7,'Total','TBLR', 0, 'C', true);
+         $pdf->Ln(7);
+
+         $pdf->SetTextColor(0,0,0);
+         $total = 0.00;
+        foreach ($productos as $row) {
+            $total = $total +$row['subTotal'];
+            $pdf->cell(45, 7, $row['fechaVenta'] ,'TBLR', 0, 'C');
+            $pdf->cell(50, 7, $row['nombreCompleto'] ,'TBLR', 0, 'C');
+            $pdf->cell(30, 7, utf8_decode($row['descripcion']),'TBLR', 0, 'C');
+            $pdf->cell(18, 7, $row['cantidad'],'TBLR', 0, 'C');
+            $pdf->cell(25, 7, $row['precio'],'TBLR', 0, 'C');
+            $pdf->cell(20, 7, number_format($row['subTotal'], 2, '.', ','),'TBLR', 1, 'C');
+        }
+        $pdf->Ln(7);
+        
+        $pdf->cell(155, 7, number_format($total, 2, '.', ','), 0, 1, 'R');
+        $pdf->cell(160, 7, 'Total pagar', 0, 1, 'R');
+        $pdf->Output();
+    }
+    public function historialVentas()
+    {
+        $this->views->getView($this, "historialVenta");
+    }
+    public function listarHistorialVentas()
+    {
+        $data = $this->model->getHistorialVentas();
+        for ($i=0; $i < count($data); $i++) { 
+            $data[$i]['acciones'] = '<div>
+            <a class="btn btn-danger" href="'.base_url. "Ventas/generarPDF/".$data[$i]['idVenta'].'" target="_blank" ><i class="fas fa-file-pdf"></i></a>
+            <div/>';
+            
+        }
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        die();
     }
 }
