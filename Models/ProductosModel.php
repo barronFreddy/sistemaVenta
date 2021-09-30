7cfa89aee20d03dbeea32d24c6050753966e0968
barronFreddy <freddybarron7834@gmail.com>
2021-09-30 16:50:44 -0400

    modificando model de productos

--- Models/ProductosModel.php
+++ Models/ProductosModel.php
@@ -1,7 +1,7 @@
 <?php
 class ProductosModel extends Query{
 
-    private $codigo,$modelo,$precioVenta,$precioCompra,$idMarca,$idCategoria,$idProducto,$estado, $idUsuario;
+    private $codigo,$modelo,$stock,$precioVenta,$precioCompra,$idMarca,$idCategoria,$idProducto,$estado, $idUsuario;
     public function __construct()
     {
         parent::__construct();
@@ -27,10 +27,11 @@ class ProductosModel extends Query{
         $data = $this->selectAll($sql);
         return $data;
     }
-    public function registrarProductos(string $codigo, string $modelo, string $precioVenta, string $precioCompra, int $idMarca, int $idCategoria, int $idUsuario)
+    public function registrarProductos(string $codigo, string $modelo,int $stock, string $precioVenta, string $precioCompra, int $idMarca, int $idCategoria, int $idUsuario)
     {
         $this->codigo = $codigo;
         $this->modelo = $modelo;
+        $this->stock = $stock;
         $this->precioVenta = $precioVenta;
         $this->precioCompra = $precioCompra;
         $this->idMarca = $idMarca;
@@ -41,8 +42,8 @@ class ProductosModel extends Query{
         $existe = $this->select($verificar);
 
         if (empty($existe)) {
-            $sql = "INSERT INTO productos(codigo, modelo, precioVenta, precioCompra, idMarca, idCategoria, idUsuario) VALUES (?,?,?,?,?,?,?)";
-            $datos = array($this->codigo, $this->modelo, $this->precioVenta, $this->precioCompra, $this->idMarca, $this->idCategoria, $this->idUsuario);
+            $sql = "INSERT INTO productos(codigo, modelo, stock, precioVenta, precioCompra, idMarca, idCategoria, idUsuario) VALUES (?,?,?,?,?,?,?,?)";
+            $datos = array($this->codigo, $this->modelo, $this->stock, $this->precioVenta, $this->precioCompra, $this->idMarca, $this->idCategoria, $this->idUsuario);
             $data=$this->save($sql,$datos);
             if ($data == 1) {
                 $res = "ok";
@@ -62,10 +63,11 @@ class ProductosModel extends Query{
         return $data;
     }
     
-    public function modificarProductos(string $codigo, string $modelo, string $precioVenta, string $precioCompra, int $idMarca, int $idCategoria, int $idProducto, int $idUsuario)
+    public function modificarProductos(string $codigo, string $modelo, int $stock, string $precioVenta, string $precioCompra, int $idMarca, int $idCategoria, int $idProducto, int $idUsuario)
     {
         $this->codigo = $codigo;
         $this->modelo = $modelo;
+        $this->stock = $stock;
         $this->precioVenta = $precioVenta;
         $this->precioCompra = $precioCompra;
         $this->idMarca = $idMarca;
@@ -73,8 +75,8 @@ class ProductosModel extends Query{
         $this->idUsuario = $idUsuario;
         $this->idProducto = $idProducto;
 
-            $sql = "UPDATE productos SET codigo = ?, modelo=?, precioVenta=?, precioCompra=?, idMarca=?, idCategoria=?, idUsuario=? WHERE idProducto =?";
-            $datos = array($this->codigo, $this->modelo, $this->precioVenta, $this->precioCompra, $this->idMarca, $this->idCategoria, $this->idUsuario, $this->idProducto);
+            $sql = "UPDATE productos SET codigo = ?, modelo=?, stock=?, precioVenta=?, precioCompra=?, idMarca=?, idCategoria=?, idUsuario=? WHERE idProducto =?";
+            $datos = array($this->codigo, $this->modelo, $this->stock, $this->precioVenta, $this->precioCompra, $this->idMarca, $this->idCategoria, $this->idUsuario, $this->idProducto);
             $data=$this->save($sql,$datos);
             if ($data == 1) {
                 $res = "modificado";
@@ -91,6 +93,12 @@ class ProductosModel extends Query{
         $sql = "UPDATE productos SET estado = ? WHERE idProducto = ?";
         $datos = array($this->estado, $this->idProducto);
         $data = $this->save($sql,$datos);
+        return $data;
+    }
+    public function consultarDetalle(int $idProducto)
+    {
+        $sql = "SELECT * FROM productos WHERE idProducto = $idProducto";
+        $data = $this->select($sql);
         return $data;
     }
 }
