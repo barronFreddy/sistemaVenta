c7b11f35f4cc87fb114dcfb69c3fa627a30f5795
barronFreddy <freddybarron7834@gmail.com>
2021-09-30 16:57:48 -0400

    modificando template header

--- Views/Templates/header.php
+++ Views/Templates/header.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Datatable - srtdash</title>
+    <title>Sistema</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--link rel="shortcut icon" type="image/png" href="a<?php echo base_url;?>asset/images/icon/favicon.ico"-->
     <link rel="stylesheet" href="<?php echo base_url;?>asset/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url;?>asset/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo base_url;?>asset/css/themify-icons.css">
@@ -15,19 +14,21 @@
     <link rel="stylesheet" href="<?php echo base_url;?>asset/css/slicknav.min.css">
     <!-- amcharts css -->
     <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+   
     <!-- Start datatable css -->
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url; ?>Assets/css/jquery.dataTables.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url; ?>Assets/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url; ?>Assets/css/responsive.bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url; ?>Assets/css/responsive.jqueryui.min.css">
-    <!-- style css -->
-    <link rel="stylesheet" href="<?php echo base_url;?>asset/css/typography.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url;?>Assets/DataTables/datatables.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url;?>Assets/DataTables/datatables.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url;?>Assets/css/responsive.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url;?>Assets/css/responsive.jqueryui.min.css">
+    <!-- style css --><link rel="stylesheet" href="<?php echo base_url;?>asset/css/typography.css">
     <link rel="stylesheet" href="<?php echo base_url;?>asset/css/default-css.css">
     <link rel="stylesheet" href="<?php echo base_url;?>asset/css/styles.css">
     <link rel="stylesheet" href="<?php echo base_url;?>asset/css/responsive.css">
     <link href="<?php echo base_url; ?>Assets/css/styles.css" rel="stylesheet" />
     <!--link href="<?php echo base_url; ?>Assets/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" /-->
     <script src="<?php echo base_url; ?>Assets/js/all.min.js" crossorigin="anonymous"></script>
+    
+    <link href="<?php echo base_url;?>Assets/css/select2.min.css" rel="stylesheet" />
     <!-- modernizr css -->
     <script src="<?php echo base_url;?>asset/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
@@ -45,11 +46,6 @@
     <div class="page-container">
         <!-- sidebar menu area start -->
         <div class="sidebar-menu">
-            <div class="sidebar-header">
-                <div class="logo">
-                    <a href="index.html"><img src="<?php echo base_url;?>asset/images/icon/logo.png" alt="logo"></a>
-                </div>
-            </div>
             <div class="main-menu">
                 <div class="menu-inner">
                     <nav>
@@ -59,7 +55,6 @@
                                     <span>Configuración</span></a>
                                 <ul class="collapse">
                                     <li><a href="<?php echo base_url;?>Usuarios">Usuarios</a></li>
-                                    <li><a href="table-layout.html"></a></li>
                                 </ul>
                             </li>
                             <li><a href="<?php echo base_url;?>Clientes"><i class="fas fa-users"></i> <span>Clientes</span></a></li>
@@ -73,7 +68,22 @@
                                 </ul>
                             </li>
                             <li><a href="<?php echo base_url;?>Proveedores"><i class="fas fa-dolly"></i><span>Proveedores</span></a></li>
-                            <li><a href="<?php echo base_url;?>Ventas"><i class="fas fa-dolly"></i><span>Ventas</span></a></li>
+                            <!--li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="fas fa-solid fa-cart-arrow-down"></i>
+                                    <span>compra</span></a>
+                                <ul class="collapse">
+                                    <li><a href="<?php echo base_url;?>Compras">Nueva compra</a></li>
+                                    <li><a href="<?php echo base_url;?>Compras/historialCompras">Historial de compra</a></li>
+                                </ul>
+                            </li-->
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="fas fa-solid fa-cart-arrow-down"></i>
+                                    <span>Ventas</span></a>
+                                <ul class="collapse">
+                                    <li><a href="<?php echo base_url;?>Ventas">Nueva venta</a></li>
+                                    <li><a href="<?php echo base_url;?>Ventas/historialVentas">Historial de ventas</a></li>
+                                </ul>
+                            </li>
                         </ul>
                     </nav>
                 </div>
@@ -102,10 +112,28 @@
                             <h4 class="user-name dropdown-toggle" data-toggle="dropdown"><i class="fa fa-angle-down"></i></h4>
                             <div class="dropdown-menu">
                                 <a class="dropdown-item" href="#">Message</a>
-                                <a class="dropdown-item" href="#">Settings</a>
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#cambiarCotrasena">Cambiar Contraseña</a>
                                 <a class="dropdown-item" href="<?php echo base_url; ?>Usuarios/salir">cerrar sesion</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+       
+        <!-- main content area start 
+        <div class="main-content">
+            page title area start 
+            <div class="page-title-area">
+                <div class="row align-items-center">
+                    <div class="col-sm-6 clearfix">
+                        <div class="user-profile pull-right">
+                            <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
+                            <h4 class="user-name dropdown-toggle" data-toggle="dropdown"><i class="fa fa-angle-down"></i></h4>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#cambiarCotrasena">Cambiar Contraseña</a>
+                                <a class="dropdown-item" href="<?php echo base_url; ?>Usuarios/salir">cerrar sesion</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>-->
