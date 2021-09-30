15ea9735b770f7ed5b4ea59bba6a04bac6a1c86d
barronFreddy <freddybarron7834@gmail.com>
2021-09-30 16:44:04 -0400

    añadiendo vista de historial ventas

---
+++ Views/Ventas/historialVenta.php
@@ -0,0 +1,22 @@
+<?php include "Views/Templates/header.php";?>
+<div class="main-content-inner mt-2">
+    <div class="card card-body">
+            <h4 class="header-title">Ventas</h4>
+            <div class="table-responsive">
+                <table class="table table-secondary" id="tblHistorialVenta">
+                    <thead class="bg-secondary text-capitalize">
+                        <tr>
+                            <th>#</th>
+                            <th>#</th>
+                            <th>Total</th>
+                            <th>Fecha Venta</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+    </div>
+</div>
+<?php include "Views/Templates/footer.php";?>
